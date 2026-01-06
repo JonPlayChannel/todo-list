63acca525400a13c1c1f3ec58d1a68e9abb95edd
JonPlayChannel <165782158+JonPlayChannel@users.noreply.github.com>
2026-01-06 19:39:21 +0300

    Added form.js to git

---
+++ src/form.js
@@ -0,0 +1,27 @@
+import { appendTasks } from "./list";
+import selectors from "./selectors";
+import { setTasksToLocalStorage } from "./storage";
+
+const todoInputElement = document.querySelector(selectors.todoInput);
+
+const addTask = () => {
+  const label = todoInputElement.value.trim();
+  if (!label) {
+    alert("Пустое имя задачи.");
+    return;
+  }
+
+  const newTask = {
+    id: crypto.randomUUID() ?? new Date(),
+    label: label,
+    isDone: false
+  };
+
+  setTasksToLocalStorage(newTask);
+  todoInputElement.value = '';
+  todoInputElement.blur();
+}
+
+export {
+  addTask
+}
